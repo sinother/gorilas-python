0e447324a2dce2f81ca5b42cadd52a67bce09d54
sinother <marcos.serrano@gmail.com>
2020-09-13 12:05:29 +0200

    terminamos todos los edificios

--- inicial.py
+++ inicial.py
@@ -17,6 +17,11 @@ screen = pygame.display.set_mode(size)
 PrimerEdificio = random.randint (50, 450)
 SegundoEdificio = random.randint (50, 450)
 TercerEdificio = random.randint (50, 450)
+CuartoEdificio = random.randint (50, 450)
+QuintoEdificio = random.randint (50, 450)
+SextoEdificio = random.randint (50, 450)
+SeptimoEdificio = random.randint (50, 450)
+
 
 while True:
     for event in pygame.event.get():
@@ -30,7 +35,10 @@ while True:
     pygame.draw.rect(screen, BLACK, (25, 500, 80, -PrimerEdificio))
     pygame.draw.rect(screen, BLACK, (130, 500, 80, -SegundoEdificio))
     pygame.draw.rect(screen, BLACK, (235, 500, 80, -TercerEdificio))
-
+    pygame.draw.rect(screen, BLACK, (340, 500, 80, -CuartoEdificio))
+    pygame.draw.rect(screen, BLACK, (445, 500, 80, -QuintoEdificio))
+    pygame.draw.rect(screen, BLACK, (550, 500, 80, -SextoEdificio))
+    pygame.draw.rect(screen, BLACK, (655, 500, 80, -SeptimoEdificio))
 
     #ZONA DE DIBUJO
 
